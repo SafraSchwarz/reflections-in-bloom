ac57e8a782ee4c57601265349d64e31881523de8
Davon Belvin <davonbelvin@gmail.com>
2021-09-06 10:33:41 -0700

    added buttons for menu added some styling to them and a hover effect

--- src/main/frontend/components/NavBar.js
+++ src/main/frontend/components/NavBar.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Switch, Route, Link, Redirect } from "react-router-dom";
 
 import LandingPage from "./LandingPage";
+import NavItem from "./NavItem";
 
 const NavBar = (props) => {
   return (
     <div className="navbar">
       <nav className="navbar">
-        <ul className="navbar-nav"></ul>
+        <ul className="navbar-nav">
+          <NavItem icon=":)"></NavItem>
+        </ul>
       </nav>
       <Switch>
         <Route exact path="/" component={LandingPage} />
---
+++ src/main/frontend/components/NavItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavItem = (props) => {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  );
+};
+
+export default NavItem;
