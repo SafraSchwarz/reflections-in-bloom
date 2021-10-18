2c099df2e9b6334c5353436a14e6cbd945dc8865
Davon Belvin <davonbelvin@gmail.com>
2021-10-17 19:33:08 -0700

    seeder is finished

--- src/main/java/com/reflectionsinbloom/ecommerce/seeders/FloralProductSeeder.java
+++ src/main/java/com/reflectionsinbloom/ecommerce/seeders/FloralProductSeeder.java
@@ -49,7 +49,41 @@ public class FloralProductSeeder {
 
           product4.setType("bouquet");
           product4.setSeason("Spring");
+          product4.setPhotoUrl("https://www.reflectionsinbloom.com/uploads/1/3/2/1/132150881/s253712914428950176_p2_i29_w3024.jpeg?width=2560?width=800");
+          product4.setSize("large");
 
+          floralProductService.save(product4);
+
+          product5.setType("wreath");
+          product5.setSeason("all");
+          product5.setPhotoUrl("https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/194009398_3997734760273822_1464765390236568283_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=973b4a&_nc_ohc=7BFkCJoO4DsAX95Plhb&_nc_ht=scontent-sea1-1.xx&oh=528208b7d56f34c6aacd535f16226215&oe=61910B5A");
+          product5.setSize("medium");
+
+          floralProductService.save(product5);
+
+          product6.setType("arrangement");
+          product6.setSeason("fall");
+          product6.setEvent("halloween");
+          product6.setSize("medium");
+          product6.setPhotoUrl("https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/122991728_3395555683825069_722256899557176015_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=jymbx93JmigAX8OZ3Pi&_nc_ht=scontent-sea1-1.xx&oh=6eb4e5998d3c7581cc6897508e7c235f&oe=619296D1");
+
+          floralProductService.save(product6);
+
+          product7.setType("hand tie");
+          product7.setSeason("spring");
+          product7.setPhotoUrl("https://scontent-sea1-1.xx.fbcdn.net/v/t1.18169-9/16114209_1256374717743187_3117994552490529186_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=973b4a&_nc_ohc=vncv5bZeRC0AX97RjsW&_nc_ht=scontent-sea1-1.xx&oh=b7035bde4bff400ca8fa94f12699a022&oe=6193D709");
+          product7.setEvent("wedding");
+          product7.setSize("large");
+
+          floralProductService.save(product7);
+
+          product8.setType("hand tie");
+          product8.setSeason("summer");
+          product8.setEvent("wedding");
+          product8.setSize("medium");
+          product8.setPhotoUrl("https://scontent-sea1-1.xx.fbcdn.net/v/t1.18169-9/16602844_1256073301106662_3429043164110603303_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=973b4a&_nc_ohc=ljVTcSzp2A0AX9rsKQk&_nc_ht=scontent-sea1-1.xx&oh=56e06a9a106876944f5ebc6b7e8af87f&oe=6191BA45");
+
+          floralProductService.save(product8);
         }
     }
 }
